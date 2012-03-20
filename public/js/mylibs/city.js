6a267bbfb4e7bfd6b7229e766b7602994857debf
Carol Nichols <carol.nichols@gmail.com>
2012-03-20 10:01:40 -0400

    Remove the toggle link on direct flights

--- public/js/mylibs/city.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $(document).ready(function(){
-    $("#show-direct-flights, #hide-direct-flights").click(function(event){
-      event.preventDefault();
-      $("#direct-flights").toggle();
-    });
-  });
-});
